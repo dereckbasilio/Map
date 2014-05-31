adc92e734f771b56d88caeac8a295c9c55521233
Dereck Basilio <dereck@docufy.com>
2014-05-31 18:39:29 -0500

    Refactor code move X in a single function to be called on keyDown

--- map.js
+++ map.js
@@ -34,37 +34,30 @@ $(document).ready(function(){
 	grid = generateGrid(15,15);
 	displayGrid(grid);
 
+	var changePosition = function(position, box, addOrSub){
+		box.html("");
+
+		if(addOrSub) currentPosition[position] += 1;
+		else currentPosition[position] -= 1;
+
+		$("#" + currentPosition[0] + "_" + currentPosition[1] ).html("X");
+	};
+
 	$(document).keydown(function(e){
 		var box = $("#" + currentPosition[0] + "_" + currentPosition[1]);
 		console.log(box);
 		switch(e.which){
 			case leftKey: 
-				if(currentPosition[1] > 1){
-					box.html("");
-					currentPosition[1] -= 1;
-					$("#" + currentPosition[0] + "_" + currentPosition[1] ).html("X");
-				}
+				if(currentPosition[1] > 1) changePosition(1, box, false);
 				break;
 			case rightKey: 
-				if(currentPosition[1] < grid[0].length){
-					box.html("");
-					currentPosition[1] += 1;
-					$("#" + currentPosition[0] + "_" + currentPosition[1] ).html("X");
-				}
+				if(currentPosition[1] < grid[0].length) changePosition(1, box, true);
 				break;
 			case upKey: 
-				if(currentPosition[0] > 1){
-					box.html("");
-					currentPosition[0] -= 1;
-					$("#" + currentPosition[0] + "_" + currentPosition[1] ).html("X");
-				}
+				if(currentPosition[0] > 1) changePosition(0, box, false);
 				break;
 			case downKey: 
-				if(currentPosition[0] < grid.length){
-					box.html("");
-					currentPosition[0] += 1;
-					$("#" + currentPosition[0] + "_" + currentPosition[1] ).html("X");
-				}
+				if(currentPosition[0] < grid.length) changePosition(0, box, true);
 				break;
 		}
 	});
