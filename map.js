03543edc381e31359b2847c4bc789c852fef1f88
Dereck Basilio <dereck@docufy.com>
2014-05-31 19:43:47 -0500

    Enemy O will move when you move

--- map.js
+++ map.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 	var size = parseInt($(".container").css("height").replace("px",""));
 
-	var currentPosition = [1,1];
-
 	var leftKey = 37;
 	var upKey = 38;
 	var rightKey = 39;
@@ -31,36 +29,58 @@ $(document).ready(function(){
 		$(".container").css("font-size", size/(grid.length*2));
 	};
 
-	grid = generateGrid(15,15);
-	displayGrid(grid);
-
 	var changePosition = function(position, box, addOrSub){
 		box.html("");
 
-		if(addOrSub) currentPosition[position] += 1;
+		if(addOrSub === 1) currentPosition[position] += 1;
 		else currentPosition[position] -= 1;
 
-		box = $("#" + currentPosition[0] + "_" + currentPosition[1] );
+		box = $("#" + currentPosition[0] + "_" + currentPosition[1]);
 		box.html("X");
 		box.css("background-color", "green");
 	};
 
+	var moveOppenent = function(position, box, addOrSub){
+		box.html("");
+
+		if(addOrSub === 1) currentEnemyPosition[position] += 1;
+		else currentEnemyPosition[position] -= 1;
+
+		box = $("#" + currentEnemyPosition[0] + "_" + currentEnemyPosition[1]);
+		box.html("O");
+		box.css("background-color", "blue");
+	};
+
 	$(document).keydown(function(e){
 		var box = $("#" + currentPosition[0] + "_" + currentPosition[1]);
-		console.log(box);
+		
+		var pos = Math.floor(Math.random() * 2);
+		var addOrSub = Math.floor(Math.random() * 2);
+		moveOppenent(pos, box, addOrSub);
+		
 		switch(e.which){
 			case leftKey: 
-				if(currentPosition[1] > 1) changePosition(1, box, false);
+				if(currentPosition[1] > 1) changePosition(1, box, 0);
 				break;
 			case rightKey: 
-				if(currentPosition[1] < grid[0].length) changePosition(1, box, true);
+				if(currentPosition[1] < grid[0].length) changePosition(1, box, 1);
 				break;
 			case upKey: 
-				if(currentPosition[0] > 1) changePosition(0, box, false);
+				if(currentPosition[0] > 1) changePosition(0, box, 0);
 				break;
 			case downKey: 
-				if(currentPosition[0] < grid.length) changePosition(0, box, true);
+				if(currentPosition[0] < grid.length) changePosition(0, box, 1);
 				break;
 		}
+		
+		pos = Math.floor(Math.random() * 2);
+		addOrSub = Math.floor(Math.random() * 2);
+		moveOppenent(pos, box, addOrSub);
 	});
+	
+	grid = generateGrid(15,15);
+	displayGrid(grid);
+
+	var currentPosition = [1,1];
+	var currentEnemyPosition = [grid.length, grid.length];
 });
